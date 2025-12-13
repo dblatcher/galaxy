429dcb38343c8bc5b0e4cd28844d963277d570e2
DBlatcher <Dv.bltchr@gmail.com>
2025-12-13 16:08:47 +0000

    fix selections

--- src/components/designer.tsx/EquipmentSelect.tsx
+++ src/components/designer.tsx/EquipmentSelect.tsx
@@ -1,10 +1,11 @@
-import { ALL_EQUIPMENT, type EquipmentId } from "../../data/ship-equipment"
+import { ALL_EQUIPMENT, equipmentIds, type EquipmentId } from "../../data/ship-equipment"
 
 interface Props {
     value: EquipmentId | undefined
     setValue: { (value: EquipmentId | undefined): void }
 }
 
+const UNDEF_TOKEN = "-_-"
 
 export const EquipmentSelect = ({ value, setValue }: Props) => {
 
@@ -12,8 +13,11 @@ export const EquipmentSelect = ({ value, setValue }: Props) => {
 
     return <div>
         <span>{equipment?.name ?? '[empty]'}</span>
-        <select value={value} onChange={({ target }) => setValue(target.value as EquipmentId | undefined)}>
-            <option value={undefined} >[none]</option>
+        <select value={value} onChange={({ target }) => {
+            const newValue = equipmentIds.includes(target.value as EquipmentId) ? target.value as EquipmentId : undefined;
+            setValue(newValue)
+        }}>
+            <option value={UNDEF_TOKEN} >[none]</option>
             {Object.entries(ALL_EQUIPMENT).map(([id, equipment]) => (
                 <option value={id} key={id}>{equipment.name}</option>
             ))}
